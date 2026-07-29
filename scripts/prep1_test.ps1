73f5599a3f59cfe9c9fd88049b97bc70ded879ef
Fernando L. O. Costa <fernandolocos@gmail.com>
2026-07-28 23:15:25 -0300

    Fix repository path in prep1_test.ps1

--- scripts/prep1_test.ps1
+++ scripts/prep1_test.ps1
@@ -87,7 +87,7 @@ foreach ($dir in $dirs) {
 Write-Host ""
 Write-Host "--- Repositorio do Curso ---" -ForegroundColor Yellow
 
-$repoPath = "C:\6258-soc-resposta-incidentes.git"
+$repoPath = "C:\6258-soc-resposta-incidentes"
 $repoExists = Test-Path "$repoPath\README.md"
 
 Test-Check -Description "Repositorio clonado em $repoPath" `
